d62d29368bb87a388e8629b05f0b85ef847e7d88
DHL-Sheffield <dhlee1@sheffield.ac.uk>
2024-11-01 16:59:05 +0000

    committing lcdm data

---
+++ lcdm_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --time=72:00:00
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+#SBATCH --mail-user=dhlee1@sheffield.ac.uk
+
+module load Anaconda3/2019.07
+source activate cosmos
+module load OpenMPI/4.0.3-GCC-9.3.0
+module load OpenBLAS/0.3.9-GCC-9.3.0
+module load CFITSIO/3.48-GCCcore-9.3.0
+
+source /users/smp24dhl/cosmo/code/planck/clik/bin/clik_profile.sh
+
+mpirun -np 4 cobaya-run lcdm.yaml
